766ca3d08b066fbfc1d64aa06dc6285cb1c5945d
OverheardAI Team <overheardai.project@gmail.com>
2025-09-27 12:16:09 +0300

    Update save.js

--- js/save.js
+++ js/save.js
@@ -90,7 +90,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Hammer Card</title>
+  <title>Hammer Site</title>
   <link rel="stylesheet" href="css/main.css" />
   <link rel="stylesheet" href="css/app.css" />
 </head>
@@ -154,3 +154,4 @@
     });
   }
 })();
+
